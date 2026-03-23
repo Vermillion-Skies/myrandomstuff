50c5bacd0154230aa0cb734bb3d03acb80649a45
Vermillion-Skies <mario4672yt@gmail.com>
2026-03-23 08:28:43 -0400

    Added custom status 1

--- Python/3. RPC bot/presencebot.py
+++ Python/3. RPC bot/presencebot.py
@@ -13,20 +13,37 @@ def getappid():
         return("fail")
         pass
     pass
+def statcode(x, y):
+    if x == str("1"):
+        if y == str("d"):
+            return("Gloria is at class rn")
+            pass
+        elif y == str("s"):
+            return("She may be slow to respond, please be patient")
+            pass
+        elif y == str("i"):
+            return("schoolimg")
+            pass
+        pass
+    pass
 appidl = getappid()
 appid = appidl[0]
 RPC = Presence(appid)
 RPC.connect()
+stat = str(input("Enter status code: "))
 print("Bot is starting, attempting to connect...")
-while True:
+botconn = str("y")
+while botconn == str("y"):
     try:
         RPC.update(
-            details="Gloria is testing an RPC bot",
-            state="RPC state test",
+            details=statcode(stat, "d"),
+            state=statcode(stat, "s"),
+            large_image=statcode(stat, "i"),
             start=time.time()
         )
         pass
     except Exception as e:
         print("An exception has occurred: " + e)
+        botconn = str("n")
         pass
     time.sleep(15)
